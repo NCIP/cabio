b8e46884566b7b5ed022cd5c7c2609a60b3674ee
Konrad Rokicki <rokickik>
2008-09-25 15:16:05 +0000

    added test for global queries
    SVN-Revision: 153

--- test/src/gov/nih/nci/cabio/portal/AllTests.java
+++ test/src/gov/nih/nci/cabio/portal/AllTests.java
@@ -7,7 +7,7 @@ import junit.framework.Test;
 import junit.framework.TestSuite;
 
 /**
- * A test suite with all the unit tests for caBIO.
+ * A test suite with all the unit tests for the caBIO portlet.
  * 
  * @author <a href="mailto:rokickik@mail.nih.gov">Konrad Rokicki</a>
  */
@@ -39,6 +39,7 @@ public class AllTests {
         TestSuite suite = new TestSuite("Test for gov.nih.nci.cabio.portal");
         //$JUnit-BEGIN$
         suite.addTestSuite(ReportTest.class);
+        suite.addTestSuite(GlobalQueriesTest.class);
         //$JUnit-END$
         return suite;
     }
---
+++ test/src/gov/nih/nci/cabio/portal/GlobalQueriesTest.java
@@ -0,0 +1,48 @@
+package gov.nih.nci.cabio.portal;
+
+import gov.nih.nci.cabio.domain.Chromosome;
+import gov.nih.nci.cabio.domain.Taxon;
+import gov.nih.nci.cabio.portal.portlet.GlobalQueries;
+import gov.nih.nci.cabio.util.ORMTestCase;
+
+import java.util.List;
+
+/**
+ * Test the global queries which are executed at portlet startup time.
+ * 
+ * @author <a href="mailto:rokickik@mail.nih.gov">Konrad Rokicki</a>
+ */
+public class GlobalQueriesTest extends ORMTestCase {
+
+    private GlobalQueries q;
+    
+    @Override
+    protected void setUp() throws Exception {
+        this.q = new GlobalQueries();
+    }
+
+    public void testAssemblyValues() throws Exception {
+        assertNotNull(q.getAssemblyValues());
+    }
+
+    public void testClassFilterValues() throws Exception {
+        assertNotNull(q.getClassFilterValues());
+    }
+
+    public void testTaxonValues() throws Exception {
+        assertNotNull(q.getTaxonValues());
+        assertEquals(2,q.getTaxonValues().size());
+    }
+
+    public void testMicroarrays() throws Exception {
+        assertNotNull(q.getMicroarrays());
+    }
+
+    public void testTaxonChromosomes() throws Exception {
+        assertNotNull(q.getTaxonChromosomes());
+        for(Taxon t : q.getTaxonValues()) {
+            List<Chromosome> chroms = q.getTaxonChromosomes().get(t);
+            assertNotNull(chroms);
+        }
+    }
+}
