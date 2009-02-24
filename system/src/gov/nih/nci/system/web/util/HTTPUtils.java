feb5a78c4022399305537181c4b7fc8c764119bd
Konrad Rokicki <rokickik>
2009-02-24 16:27:53 +0000

    Rollback fix for GF14559, which is no longer needed because of the upgrade to SDK 4.1. The GF14559 fix broke the pageNumber parameter in caBIO 4.2, so rolling back will fix that.
    SVN-Revision: 415

--- system/src/gov/nih/nci/system/web/util/HTTPUtils.java
+++ system/src/gov/nih/nci/system/web/util/HTTPUtils.java
@@ -835,9 +835,9 @@ public class HTTPUtils implements Serializable{
 			if(startIndex != null || !startIndex.equals("0")){
 				index = Integer.parseInt(startIndex);
 			}
-			if(pageSize != null){
-                counter = Integer.parseInt(pageSize);
-			}
+            if(resultCounter != null){
+                counter = Integer.parseInt(resultCounter);
+            }
 			if (roleName != null){
 				results = applicationService.getAssociation(criteria, roleName);
 			} else {
@@ -850,11 +850,18 @@ public class HTTPUtils implements Serializable{
 			log.error("Exception: ", ex);
 			throw ex;
 		}
+        
+        if((counter + index) > results.size()){
+            counter = results.size();
+        }
+        else{
+            counter += index;
+        }
 
-		Object[] resultSet = new Object[counter];
-		for(int i=index, s=0; i<results.size() && s<counter; i++,s++){
-			resultSet[s] = results.get(i);
-		}
+        Object[] resultSet = new Object[counter];
+        for(int i = index, s=0; i< counter; i++,s++){
+            resultSet[s]= results.get(i);
+        }
 
 		return resultSet;
 	}
