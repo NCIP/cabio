0848cbf42fb82badc236b982230936d63322cef6
Konrad Rokicki <rokickik>
2009-02-25 21:13:50 +0000

    enabled all tests
    SVN-Revision: 417

--- cabioapi/test/src/gov/nih/nci/cabio/AllTests.java
+++ cabioapi/test/src/gov/nih/nci/cabio/AllTests.java
@@ -40,7 +40,7 @@ public class AllTests {
     public static Test suite() {
         TestSuite suite = new TestSuite("Test for gov.nih.nci.cabio");
         //$JUnit-BEGIN$
-        //suite.addTestSuite(AnnotationAPITest.class);
+        suite.addTestSuite(AnnotationAPITest.class);
         suite.addTestSuite(ArraysTest.class);
         suite.addTestSuite(CaBIGStandardModelTest.class);
         suite.addTestSuite(CGDCTest.class);
@@ -48,7 +48,7 @@ public class AllTests {
         suite.addTestSuite(DefectTest.class);
         suite.addTestSuite(DevGuideTest.class);
         suite.addTestSuite(FreestyleLMTest.class);
-        //suite.addTestSuite(GridIdTest.class);
+        suite.addTestSuite(GridIdTest.class);
         suite.addTestSuite(MarkerTest.class);
         suite.addTestSuite(PIDTest.class);
         suite.addTestSuite(QueryTest.class);
