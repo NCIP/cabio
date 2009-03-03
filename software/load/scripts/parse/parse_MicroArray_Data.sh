70e09e3272b3b7876981cbc4fa7d7122f6dbec47
Lalitha Viswanath <viswanathl>
2009-03-03 18:00:20 +0000

    changes for u133b and u95 parser
    SVN-Revision: 454

--- software/load/scripts/parse/parse_MicroArray_Data.sh
+++ software/load/scripts/parse/parse_MicroArray_Data.sh
@@ -6,13 +6,25 @@ echo "Parsing Affymetrix HG-U133_Plus2 MicroArray data"
 cd "$CABIO_DIR"/scripts/parse/arrays/Affymetrix/HG-U133_Plus2
 sh Affymetrix_HG_U133_Plus2_DataParser.sh
 
-echo "Parsing Affymetrix HG-U133 MicroArray data"
+echo "Parsing Affymetrix HG-U133A MicroArray data"
 cd "$CABIO_DIR"/scripts/parse/arrays/Affymetrix/HG-U133
 sh Affymetrix_HG_U133_DataParser.sh
 
 echo "Parsing Affymetrix HuMapping MicroArray data"
 cd "$CABIO_DIR"/scripts/parse/arrays/Affymetrix/HuMapping
 sh Affymetrix_HuMappingMA_DataParser.sh
+
+echo "Parsing Affymetrix HG-U95 Microarray data"
+cd "$CABIO_DIR"/scripts/parse/arrays/Affymetrix/HG-U95-U133B-6800
+sh Affy_HGU95_DataParser.sh
+
+echo "Parsing Affymetrix HG-U133B Microarray data"
+cd "$CABIO_DIR"/scripts/parse/arrays/Affymetrix/HG-U95-U133B-6800
+sh Affy_HGU133B_DataParser.sh
+
+echo "Parsing Affymetrix Hu6800 Microarray data"
+cd "$CABIO_DIR"/scripts/parse/arrays/Affymetrix/HG-U95-U133B-6800
+sh Affy_Hu6800_DataParser.sh
 
 echo "Parsing Agilent aCGH data"
 cd "$CABIO_DIR"/scripts/parse/arrays/Agilent/aCGH244K
