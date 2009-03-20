cc63c5147c76be408e2281448c237db2f42794d0
Lalitha Viswanath <viswanathl>
2009-03-20 18:36:19 +0000

    Changes for HT-HGU133B arrays
    SVN-Revision: 499

--- load/scripts/sql_loader/unigene2gene/mappingData_postprocess.sql
+++ load/scripts/sql_loader/unigene2gene/mappingData_postprocess.sql
@@ -3,6 +3,7 @@
 @$LOAD/indexes/zstg_gene2accession.cols.sql;
 @$LOAD/indexes/zstg_genealias.cols.sql;
 @$LOAD/indexes/zstg_gene2refseq.cols.sql;
+@$LOAD/indexes/zstg_gene2go.cols.sql;
 @$LOAD/indexes/zstg_seqgene.cols.sql;
 @$LOAD/indexes/zstg_seqsts.cols.sql;
 
@@ -12,6 +13,7 @@
 @$LOAD/constraints/zstg_gene2accession.enable.sql;
 @$LOAD/constraints/zstg_genealias.enable.sql;
 @$LOAD/constraints/zstg_gene2refseq.enable.sql;
+@$LOAD/constraints/zstg_gene2go.enable.sql;
 @$LOAD/constraints/zstg_seqgene.enable.sql;
 @$LOAD/constraints/zstg_seqsts.enable.sql;
 
@@ -20,16 +22,20 @@
 @$LOAD/triggers/zstg_gene2accession.enable.sql;
 @$LOAD/triggers/zstg_genealias.enable.sql;
 @$LOAD/triggers/zstg_gene2refseq.enable.sql;
+@$LOAD/triggers/zstg_gene2go.enable.sql;
 @$LOAD/triggers/zstg_seqgene.enable.sql;
 @$LOAD/triggers/zstg_seqsts.enable.sql;
 
---ANALYZE TABLE zstg_gene2UNIGENE COMPUTE STATISTICS;
---ANALYZE TABLE zstg_omim2gene COMPUTE STATISTICS;
---ANALYZE TABLE zstg_gene2ACCESSION COMPUTE STATISTICS;
---ANALYZE TABLE zstg_geneALIAS COMPUTE STATISTICS;
---ANALYZE TABLE zstg_gene2refseq COMPUTE STATISTICS;
---ANALYZE TABLE zstg_seqgene COMPUTE STATISTICS;
---ANALYZE TABLE zstg_seqsts COMPUTE STATISTICS;
+ANALYZE TABLE zstg_gene2UNIGENE COMPUTE STATISTICS;
+ANALYZE TABLE zstg_omim2gene COMPUTE STATISTICS;
+ANALYZE TABLE zstg_gene2ACCESSION COMPUTE STATISTICS;
+ANALYZE TABLE zstg_geneALIAS COMPUTE STATISTICS;
+ANALYZE TABLE zstg_gene2refseq COMPUTE STATISTICS;
+ANALYZE TABLE zstg_gene2go COMPUTE STATISTICS;
+ANALYZE TABLE zstg_seqgene COMPUTE STATISTICS;
+ANALYZE TABLE zstg_seqsts COMPUTE STATISTICS;
+
+-- Add Hugo Symbol to Gene
 UPDATE gene_tv G SET HUGO_SYMBOL = (SELECT DISTINCT SYMBOL
                                   FROM zstg_geneALIAS A, zstg_gene_identifiers B
      WHERE A.LOCUSLINKID = B.IDENTIFIER AND B.data_source = 2 AND G.gene_ID = 
