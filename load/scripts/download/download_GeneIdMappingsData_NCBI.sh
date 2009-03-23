b46bb0f5a81cf6b3ecaeb153e5ecfc33e31b39a2
Lalitha Viswanath <viswanathl>
2009-03-23 14:51:37 +0000

    SVN-Revision: 509

--- load/scripts/download/download_GeneIdMappingsData_NCBI.sh
+++ load/scripts/download/download_GeneIdMappingsData_NCBI.sh
@@ -12,12 +12,20 @@ wget -nv ftp://ftp.ncbi.nlm.nih.gov/gene/DATA/mim2gene
 wget -nv ftp://ftp.ncbi.nlm.nih.gov/gene/DATA/gene2accession.gz
 wget -nv ftp://ftp.ncbi.nih.gov/gene/DATA/gene_refseq_uniprotkb_collab.gz  	 
 wget -nv ftp://ftp.ncbi.nih.gov/gene/DATA/gene_info.gz  	 
+wget -nv ftp://ftp.ncbi.nih.gov/gene/DATA/gene2refseq.gz  	 
+wget -nv ftp://ftp.ncbi.nih.gov/gene/DATA/gene2go.gz  	 
+wget -nv ftp://ftp.ncbi.nih.gov/gene/DATA/gene2pubmed.gz  	 
 
 gunzip -f gene2accession.gz
 gunzip -f gene_info.gz
+gunzip -f gene2refseq.gz
+gunzip -f gene2go.gz
+gunzip -f gene2pubmed.gz
+gunzip gene_refseq_uniprotkb_collab.gz
 mv gene2unigene gene2unigene.txt
 mv mim2gene mim2gene.txt
 mv gene2accession gene2accession.txt
-
-gunzip gene_refseq_uniprotkb_collab.gz
+mv gene2refseq gene2refseq.txt
+mv gene2go gene2go.txt
+mv gene2pubmed gene2pubmed.txt
 mv gene_refseq_uniprotkb_collab gene_refseq_uniprotkb_collab.txt 
