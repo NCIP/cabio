d83e678b8b37b6a68a52b9168e3dc529b2d75040
Konrad Rokicki <rokickik>
2009-05-07 19:32:01 +0000

    clean up
    SVN-Revision: 641

--- src/gov/nih/nci/cabio/portal/portlet/ReportService.java
+++ src/gov/nih/nci/cabio/portal/portlet/ReportService.java
@@ -21,10 +21,8 @@ import gov.nih.nci.system.query.hibernate.HQLCriteria;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
