219dd3b56974a98a55f8dbfeeca4df349c506052
Jim Sun <sunj2>
2009-05-15 15:25:11 +0000

    Copied remotely
    SVN-Revision: 683

---
+++ cabioapi/codegen/core/src/gov/nih/nci/codegen/util/FreestyleTransformerUtils.java
@@ -0,0 +1,37 @@
+package gov.nih.nci.codegen.util;
+
+import gov.nih.nci.ncicb.xmiinout.domain.UMLClass;
+import gov.nih.nci.ncicb.xmiinout.domain.UMLAttribute;
+
+/*
+ * Created on Sep 10, 2007
+ * ShaziyaMuhsin
+ * 
+ */
+public class FreestyleTransformerUtils {
+
+    public FreestyleTransformerUtils() {
+        super();       
+    }
+    public static boolean isClassIndexed(UMLClass klass){
+        boolean indexClass = false;
+        if(klass.getTaggedValue("ca_index_class")!=null){
+            String index = klass.getTaggedValue("ca_index_class").getValue();
+            if(index.equalsIgnoreCase("true")){
+                indexClass = true;
+            }
+        }
+        return indexClass;
+    }
+    public static boolean isAttributeIndexed(UMLAttribute attr){
+        boolean indexAttribute = false;
+        if(attr.getTaggedValue("ca_index_attribute")!=null){
+            String index = attr.getTaggedValue("ca_index_attribute").getValue();
+            if(index.equalsIgnoreCase("true")){
+                indexAttribute = true;
+            }
+        }
+        return indexAttribute;
+    }
+
+}
