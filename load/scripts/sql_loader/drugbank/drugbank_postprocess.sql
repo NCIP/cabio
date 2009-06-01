f613eb026131365186ef6063def3716b0870c6d9
Konrad Rokicki <rokickik>
2009-06-01 15:35:25 +0000

    insert gene/drug associations
    SVN-Revision: 718

--- load/scripts/sql_loader/drugbank/drugbank_postprocess.sql
+++ load/scripts/sql_loader/drugbank/drugbank_postprocess.sql
@@ -1,10 +1,4 @@
 
--- Match drugs in Agent 
-
-merge into ZSTG_DRUGBANK_DRUGS d using AGENT a 
-    on (lower(a.AGENT_NAME) = lower(d.GENERIC_NAME))
-    when matched then update set d.CABIO_AGENT_ID = a.AGENT_ID;
-
 -- Match species in Taxon
 
 merge into ZSTG_DRUGBANK_TARGETS d using (select * from TAXON where PERFERED = 1) t
@@ -14,7 +8,7 @@ merge into ZSTG_DRUGBANK_TARGETS d using (select * from TAXON where PERFERED = 1
 -- Match targets in Gene
 
 insert into ZSTG_DRUGBANK_TARGET_GENES
-select t.TARGET_ID, g.GENE_ID 
+select distinct t.TARGET_ID, g.GENE_ID 
     from ZSTG_DRUGBANK_TARGETS t, GENE_TV g
     where lower(g.SYMBOL) = lower(t.GENE_NAME)
     and g.TAXON_ID = t.CABIO_TAXON_ID;
@@ -40,7 +34,7 @@ END;
 -- Merge new drug data into Agent
 
 merge into AGENT a using ZSTG_DRUGBANK_DRUGS d
-    on (a.AGENT_ID = d.CABIO_AGENT_ID)
+    on (lower(a.AGENT_NAME) = lower(d.GENERIC_NAME))
     when matched then update set
         a.DRUGBANK_ACCESSION = d.Drug_id,
         a.EVS_ID = d.EVS_Id,
@@ -99,38 +93,136 @@ commit;
 ANALYZE TABLE agent_agent_alias COMPUTE STATISTICS;
 ANALYZE TABLE agent_alias COMPUTE STATISTICS;
 
--- TODO: Insert drug gene associations
+-- Clone the Cancer Gene Index tables for caBIO 4.3 and up
 
+insert into GENE_FUNCTION_ASSOCIATION_43 
+    (ID, GENE_ID, AGENT_ID, HISTOLOGYCODE_ID, ROLE_ID, 
+     EVIDENCE_ID, DISCRIMINATOR, BIG_ID)
+select ID, GENE_ID, AGENT_ID, HISTOLOGYCODE_ID, ROLE_ID, 
+    EVIDENCE_ID, DISCRIMINATOR, BIG_ID 
+from GENE_FUNCTION_ASSOCIATION;
 
-
-
+insert into EVIDENCE_43 (ID, PUBMED_ID, NEGATION_STATUS, 
+    CELLLINE_STATUS, COMMENTS, SENTENCE_STATUS, SENTENCE, BIG_ID) 
+select ID, PUBMED_ID, NEGATION_STATUS, 
+    CELLLINE_STATUS, COMMENTS, SENTENCE_STATUS, SENTENCE, BIG_ID
+from EVIDENCE;
 
 commit;
-ANALYZE TABLE gene_function_association COMPUTE STATISTICS;
-ANALYZE TABLE gene_function_assoc_evidence COMPUTE STATISTICS;
-ANALYZE TABLE evidence COMPUTE STATISTICS;
+
+-- Populate the many-to-many correlation table introduced in 4.3
+
+insert into GENE_FUNCTION_ASSOC_EVIDENCE (GENE_FUNCTION_ASSOC_ID, EVIDENCE_ID)
+select distinct ID, EVIDENCE_ID
+from GENE_FUNCTION_ASSOCIATION_43;
+
+commit;
+
+-- Insert references
+
+DROP SEQUENCE EVIDENCE_SEQ;
+DECLARE
+    N NUMBER(10);
+BEGIN
+    SELECT MAX(ID)+1 INTO N FROM EVIDENCE;
+    Execute immediate ('CREATE SEQUENCE EVIDENCE_SEQ START WITH '||N);
+END;
+/
+
+insert into EVIDENCE_43 (ID, PUBMED_ID, SOURCE)
+select EVIDENCE_SEQ.nextVal, pubmed_id, 'Drugbank' from ( 
+    select distinct to_number(pubmed_id) pubmed_id 
+    from ZSTG_DRUGBANK_DRUG_TARGETS where pubmed_id is not null
+);
+
+commit;
+
+CREATE INDEX CABIODEV.EVIDENCE43_INDEX ON CABIODEV.EVIDENCE_43
+(PUBMED_ID, SOURCE)
+NOLOGGING
+TABLESPACE CABIO_FUT
+PCTFREE    10
+INITRANS   2
+MAXTRANS   255
+STORAGE    (
+            INITIAL          64K
+            MINEXTENTS       1
+            MAXEXTENTS       UNLIMITED
+            PCTINCREASE      0
+            BUFFER_POOL      DEFAULT
+           )
+NOPARALLEL;
+
+ANALYZE TABLE EVIDENCE_43 COMPUTE STATISTICS;
+
+-- Insert drug gene associations
+
+create or replace view ZSTG_DRUGBANK_DRUG_TARGETS_VW as
+    select distinct Target_Id, Drug_id from ZSTG_DRUGBANK_DRUG_TARGETS;
+    
+DROP SEQUENCE GFA_SEQ;
+DECLARE
+    N NUMBER(10);
+BEGIN
+    SELECT MAX(ID)+1 INTO N FROM GENE_FUNCTION_ASSOCIATION_43;
+    Execute immediate ('CREATE SEQUENCE GFA_SEQ START WITH '||N);
+END;
+/
+
+insert into GENE_FUNCTION_ASSOCIATION_43 
+    (ID, AGENT_ID, GENE_ID, ROLE_ID, DISCRIMINATOR, SOURCE)
+select GFA_SEQ.nextVal, cabio_agent_id, cabio_gene_id, 
+    'Chemical_or_Drug_Has_Target_Gene_Product','GeneAgentAssociation','Drugbank'
+from (
+    select distinct a.AGENT_ID, tg.cabio_gene_id
+    from AGENT a, ZSTG_DRUGBANK_DRUG_TARGETS_VW dt, 
+        ZSTG_DRUGBANK_TARGETS t, ZSTG_DRUGBANK_TARGET_GENES tg
+    where a.DRUGBANK_ACCESSION = dt.drug_id
+    and dt.target_id = t.target_id
+    and t.target_id = tg.target_id
+);
+
+commit;
+
+insert into GENE_FUNCTION_ASSOC_EVIDENCE (GENE_FUNCTION_ASSOC_ID, EVIDENCE_ID)
+select distinct gfa.id, e.id
+from GENE_FUNCTION_ASSOCIATION_43 gfa, ZSTG_DRUGBANK_TARGET_GENES tg, AGENT a, 
+     ZSTG_DRUGBANK_DRUG_TARGETS dt, EVIDENCE_43 e
+where gfa.gene_id = tg.cabio_gene_id
+and gfa.agent_id = a.agent_id
+and gfa.source = 'Drugbank'
+and dt.drug_id = a.DRUGBANK_ACCESSION
+and dt.target_id = tg.target_id
+and dt.pubmed_id = e.pubmed_id
+and e.source = 'Drugbank'
+
+commit;
+
+ANALYZE TABLE GENE_FUNCTION_ASSOC_EVIDENCE COMPUTE STATISTICS;
+ANALYZE TABLE GENE_FUNCTION_ASSOCIATION_43 COMPUTE STATISTICS;
+ANALYZE TABLE EVIDENCE_43 COMPUTE STATISTICS;
 
 -- enable indexes and constraints
 
-@$LOAD/indexes/gene_function_association.lower.sql
 @$LOAD/indexes/gene_function_assoc_evidence.lower.sql
-@$LOAD/indexes/evidence.lower.sql
 @$LOAD/indexes/agent.lower.sql
 @$LOAD/indexes/agent_agent_alias.lower.sql
 @$LOAD/indexes/agent_alias.lower.sql
+@$LOAD/indexes/gene_function_association_43.lower.sql
+@$LOAD/indexes/evidence_43.lower.sql
 
-@$LOAD/indexes/gene_function_association.cols.sql
 @$LOAD/indexes/gene_function_assoc_evidence.cols.sql
-@$LOAD/indexes/evidence.cols.sql
 @$LOAD/indexes/agent.cols.sql
 @$LOAD/indexes/agent_agent_alias.cols.sql
 @$LOAD/indexes/agent_alias.cols.sql
+@$LOAD/indexes/gene_function_association_43.cols.sql
+@$LOAD/indexes/evidence_43.cols.sql
 
-@$LOAD/constraints/gene_function_association.enable.sql
 @$LOAD/constraints/gene_function_assoc_evidence.enable.sql
-@$LOAD/constraints/evidence.enable.sql
 @$LOAD/constraints/agent.enable.sql
 @$LOAD/constraints/agent_agent_alias.enable.sql
 @$LOAD/constraints/agent_alias.enable.sql
+@$LOAD/constraints/gene_function_association_43.enable.sql
+@$LOAD/constraints/evidence_43.enable.sql
 
-
+EXIT;
