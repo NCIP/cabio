5318a6c724c6bd41f81e84635dcc0882a3ade868
Jim Sun <sunj2>
2010-02-22 15:47:17 +0000

    CGI limiters feature
    SVN-Revision: 1052

--- src/gov/nih/nci/cabio/portal/portlet/canned/AgentToGenesQueryForm.java
+++ src/gov/nih/nci/cabio/portal/portlet/canned/AgentToGenesQueryForm.java
@@ -3,8 +3,9 @@ package gov.nih.nci.cabio.portal.portlet.canned;
 
 /**
  * @author <a href="mailto:rokickik@mail.nih.gov">Konrad Rokicki</a>
+ * @author <a href="mailto:sunj2@mail.nih.gov">Jim Sun</a>
  */
-public class AgentToGenesQueryForm extends PaginatedForm {
+public class AgentToGenesQueryForm extends CGIBaseQueryForm {
 
     private String agent = "albumin";
 
