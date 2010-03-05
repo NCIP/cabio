2c429385181698e6d90d45928b8732aeae08036e
Scott Gustafson <gustafsons>
2010-03-05 15:20:28 +0000

    Updates for Refresh
    SVN-Revision: 1085

--- load/scripts/load_g.sh
+++ load/scripts/load_g.sh
@@ -3,7 +3,7 @@
 # Configure enviroment variables
 `source setup_env.sh`
 
-echo "Starting load " | mail -s "Starting load " viswanathl@mail.nih.gov
+echo "Starting load " | mail -s "Starting load " gustafss@mail.nih.gov
 # Download data
 echo "Starting download"
 cd $CABIO_DIR/scripts/download
@@ -19,11 +19,11 @@ cd $CABIO_DIR/scripts/sql_loader
 time sqlplus $SCHEMA\/$SCHEMA_PWD\@$SCHEMA_DB @$LOAD/all_ref_constraints.sql 1>refConstraints.log 2>&1
 time sqlplus $SCHEMA\/$SCHEMA_PWD\@$SCHEMA_DB @$LOAD/constraints/disable.referential.sql 1>>refConstraints.log 2>&1
  
-echo "Beginning PL/SQL procedures that load from CGAP (BIOPATHWAYS, LIBRARIES, etc)";
+echo "Beginning PL/SQL procedures that load from CGAP (LIBRARIES, etc)";
 time sqlplus $SCHEMA\/$SCHEMA_PWD\@$SCHEMA_DB @$LOAD/sql/cgap_dataLoad.sql 1>$LOAD/sql/cgapLoad.log 2>&1 &
 wait
 
-echo "Done download " | mail -s "Finished download and library-load " viswanathl@mail.nih.gov
+echo "Done download " | mail -s "Finished download and library-load " gustafss@mail.nih.gov
 
 # start parse of drug bank data
 #cd $CABIO_DIR/scripts/parse/drugbank
@@ -41,7 +41,7 @@ echo "Starting parse of array data"
 cd $CABIO_DIR/scripts/parse
 sh parse_MicroArray_Data.sh 1>>$parse_LOG 2>>$parse_BAD 
 
-echo "Done Parse " | mail -s "Finished parse " viswanathl@mail.nih.gov
+echo "Done Parse " | mail -s "Finished parse " gustafss@mail.nih.gov
 
 # Load Data
 echo "Beginning Data Loads"
@@ -49,7 +49,7 @@ cd $CABIO_DIR/scripts/sql_loader
 rm *.log
 rm *.bad
 
-time sh scripts_load.sh $SCHEMA\/$SCHEMA_PWD\@$SCHEMA_DB 1>>$sqlldr_LOG 2>$sqlldr_BAD  
+time sh scripts_load.sh $SCHEMA\/$SCHEMA_PWD\@$SCHEMA_DB 1>$sqlldr_LOG 2>$sqlldr_BAD  
 
-echo "Done Part 4 " | mail -s "Finished data load " viswanathl@mail.nih.gov
+echo "Done Part 4 " | mail -s "Finished data load " gustafss@mail.nih.gov
 exit
