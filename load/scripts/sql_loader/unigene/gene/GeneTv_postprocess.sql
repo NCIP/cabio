9405cce4901014954f61bd054c682befd8d927e9
Scott Gustafson <gustafsons>
2010-03-05 15:23:28 +0000

    Updates for refresh
    SVN-Revision: 1088

--- load/scripts/sql_loader/unigene/gene/GeneTv_postprocess.sql
+++ load/scripts/sql_loader/unigene/gene/GeneTv_postprocess.sql
@@ -2,9 +2,11 @@
 @$LOAD/indexes/gene_tv.cols.sql
 @$LOAD/indexes/gene_tv.lower.sql
 @$LOAD/constraints/gene_tv.enable.sql 
-
-CREATE INDEX GTVCOMP1 ON gene_tv(HUGO_SYMBOL, 1) tablespace cabio_fut;
-CREATE INDEX GTVCOMP2 ON gene_tv(SYMBOL, 1) tablespace cabio_fut;
+column columnprod new_value prod_tablspc;
+select globals.get_production_tablespace as columnprod from dual;
+CREATE INDEX GTVCOMP1 ON gene_tv(HUGO_SYMBOL, 1) tablespace &prod_tablspc;
+CREATE INDEX GTVCOMP2 ON gene_tv(SYMBOL, 1) tablespace &prod_tablspc;
+UPDATE GENE_TV SET SOURCE = 'Unigene';
 ANALYZE TABLE gene_tv COMPUTE STATISTICS;
 
 COMMIT;
