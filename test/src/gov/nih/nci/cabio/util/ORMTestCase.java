559518f581115009ef0b66b96d0fe2d5be6119b1
Konrad Rokicki <rokickik>
2010-06-28 14:39:20 +0000

    updated
    SVN-Revision: 1389

--- test/src/gov/nih/nci/cabio/util/ORMTestCase.java
+++ test/src/gov/nih/nci/cabio/util/ORMTestCase.java
@@ -21,8 +21,13 @@ public class ORMTestCase extends TestCase {
      * assertPreloaded(gene,"geneCollection")
      * assertPreloaded(gene,"taxon")
      * </code>
+     * if the association is null it will be treated as NOT preloaded. To avoid 
+     * this, the caller should first assert that the association it not null
+     * by calling the method directly, like this:
+     * assertNotNull(gene.getTaxon());
      * @param obj The domain object (a Hibernate proxy) 
      * @param roleName The role name of the association to be tested
+     * @throws AssertionFailedError
      */
     protected void assertPreloaded(Object obj, String roleName) throws Exception {
         Object orig = ReflectionUtils.upwrap(obj);
@@ -33,8 +38,9 @@ public class ORMTestCase extends TestCase {
                 assertTrue(roleName+" is not preloaded", ps.wasInitialized());
             }
             else { 
+                String n = association.getClass().getName();
                 assertFalse(roleName+" is not preloaded", 
-                    association.getClass().getName().contains("CGLIB"));
+                    n.contains("CGLIB"));
             }
         }
     }
