9d77c377f8472447debaaf01746b8c4a393c52b7
Jim Sun <sunj2>
2010-07-06 15:15:49 +0000

    remove extra attrs
    SVN-Revision: 1421

--- maservice/src/gov/nih/nci/maservice/util/SearchCriteria.java
+++ maservice/src/gov/nih/nci/maservice/util/SearchCriteria.java
@@ -8,19 +8,5 @@ import gov.nih.nci.iso21090.Ii;
  * @created 09-Jun-2010 2:21:47 PM
  */
 public class SearchCriteria {
-
-	private Ii id;
-
-	public SearchCriteria(){
-
-	}
-
-	public Ii getId() {
-		return id;
-	}
-
-	public void setId(Ii id) {
-		this.id = id;
-	}
 	
 }
