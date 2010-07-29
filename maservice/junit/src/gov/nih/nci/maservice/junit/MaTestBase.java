32435b7dca0ec206c99b9bbe2b5d0f212e183213
Jim Sun <sunj2>
2010-07-29 19:43:05 +0000

    Added some common classed into the base
    SVN-Revision: 1490

--- maservice/junit/src/gov/nih/nci/maservice/junit/MaTestBase.java
+++ maservice/junit/src/gov/nih/nci/maservice/junit/MaTestBase.java
@@ -1,8 +1,16 @@
 package gov.nih.nci.maservice.junit;
 
+import java.io.PrintStream;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import gov.nih.nci.iso21090.Cd;
+import gov.nih.nci.iso21090.St;
+import gov.nih.nci.maservice.domain.Microarray;
+import gov.nih.nci.maservice.domain.Organism;
+import gov.nih.nci.maservice.util.GeneSearchCriteria;
+import gov.nih.nci.maservice.util.ReporterSearchCriteria;
 import gov.nih.nci.system.applicationservice.MaApplicationService;
 import gov.nih.nci.system.client.ApplicationServiceProvider;
 import junit.framework.TestCase;
@@ -56,5 +64,53 @@ public abstract class MaTestBase extends TestCase {
 		return "Molecular Service Base Test Case";
 	}
 
+	protected void printCD(Cd cd, PrintStream out)
+	{
+		if ( cd!= null)
+		{
+			out.println(" Code: " + cd.getCode()
+					    + ", CodeSystem: " + cd.getCodeSystem()
+					    + ", CodeSystemName: " + cd.getCodeSystemName()
+					    + ", CodeSystemVersion: " + cd.getCodeSystemName()
+					    + ", OriginalText: " + cd.getOriginalText().getValue());
+		}
+	}
+	
+	   /**
+     * Get a GeneSearchCriteria for a given symbol and taxon.
+     * @param symbol
+     * @param taxon
+     * @return
+     */
+    protected GeneSearchCriteria getCriteria(String symbol, String taxon) {
+        GeneSearchCriteria geneSearchCriteria = new GeneSearchCriteria();
+        St symbolOrAlias = new St();
+        symbolOrAlias.setValue(symbol);
+        St commonName = new St();
+        commonName.setValue(taxon);
+        Organism organism = new Organism();
+        organism.setCommonName(commonName);
+        geneSearchCriteria.setSymbolOrAlias(symbolOrAlias);
+        geneSearchCriteria.setOrganism(organism);
+        return geneSearchCriteria;
+    }
 
+    /**
+     * Get a ReporterSearchCriteria for a given reporter and microarray name.
+     * @param symbol
+     * @param taxon
+     * @return
+     */
+    protected ReporterSearchCriteria getReporterSearchCriteria(String reporterName, String microarrayName) {
+        ReporterSearchCriteria reporterSearchCriteria = new ReporterSearchCriteria();
+        St reporterNameSt = new St();
+        reporterNameSt.setValue(reporterName);
+        St microarrayNameSt = new St();
+        microarrayNameSt.setValue(microarrayName);
+        Microarray microarray = new Microarray();
+        microarray.setName(microarrayNameSt);
+        reporterSearchCriteria.setReporterName(reporterNameSt);
+        reporterSearchCriteria.setMicroarray(microarray);
+        return reporterSearchCriteria;
+    }	
 }
