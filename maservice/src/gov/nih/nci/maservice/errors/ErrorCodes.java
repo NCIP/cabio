9051a5907b3605d878b7d7398c06479e0d127589
Jim Sun <sunj2>
2010-08-16 16:01:50 +0000

    MA Exception update
    SVN-Revision: 1550

---
+++ maservice/src/gov/nih/nci/maservice/errors/ErrorCodes.java
@@ -0,0 +1,26 @@
+package gov.nih.nci.maservice.errors;
+
+public enum ErrorCodes {
+	MAE00000("Fetal", "Remote System Error"), 
+	MAE10001("Error", "Gene doesn't exist"),
+	MAE10002("Error", "Specified microarray is not supported"),
+	MAE10003("Error", "Reporter not found");
+			
+	private ErrorCodes(String condition, String desc) {
+		this.condition = condition;
+		this.desc = desc;
+	}
+
+	private String condition = null;
+	private String desc = null;
+
+	public String getDesc() {
+		return desc;
+	}
+	
+	public String getCondition() {
+		return condition;
+	}
+	
+	
+}
