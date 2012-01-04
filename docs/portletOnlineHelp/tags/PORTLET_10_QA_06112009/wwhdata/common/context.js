5e0d60d5b30f564d2a4ccfb823f3cf6bc8e5b3e9
Sue Pan <sue.pan@nih.gov>
2012-01-04 17:38:23 +0000

    SVN-Revision: 1890

--- docs/portletOnlineHelp/tags/PORTLET_10_QA_06112009/wwhdata/common/context.js
+++
@@ -1,4 +0,0 @@
-function  WWHBookData_Context()
-{
-  return "caBIO_Portlet_Online_Help";
-}
--- docs/portletOnlineHelp/tags/PORTLET_10_RELEASE/wwhdata/common/topics.js
+++
@@ -1,6 +0,0 @@
-function  WWHBookData_MatchTopic(P)
-{
-var C=null;
-if(P=="welcome")C="Welcome%20to%20caBIO%20Portlet.1.1.html#1052621";
-return C;
-}
