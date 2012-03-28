a6e6fc8cfb4533d15069ca1ef89b7c9e0db13543
Sue Pan <sue.pan@nih.gov>
2012-03-28 17:52:49 +0000

    caBIO Release 1.2.1
    SVN-Revision: 1952

---
+++ cabio-portlets/WebRoot/WEB-INF/jsp/canned/pathwayByNameQuery.jsp
@@ -0,0 +1,20 @@
+<%@ include file="/WEB-INF/jsp/init.jsp" %>
+
+<div id="queries_pathwayByName" class="query">
+
+    <html:form action="/cabioportlet/pathwayByNameQuery">
+    <html:hidden property="page" styleId="page"/>
+
+    <table>
+
+    <tr><td title="Name of the pathway"><label for="queries_pathwayByName_pathwayName">Pathway Name</label></td><td>
+    <html:text property="pathwayName" size="30" styleId="queries_pathwayByName_pathwayName"/>
+    </td></tr>
+
+    </table>
+
+    <html:submit>Search</html:submit>
+    <html:reset>Reset</html:reset>
+
+    </html:form>
+</div>
---
+++ cabio-portlets/src/gov/nih/nci/cabio/portal/portlet/canned/ReporterByNameQueryAction.java
@@ -0,0 +1,83 @@
+package gov.nih.nci.cabio.portal.portlet.canned;
+
+import gov.nih.nci.cabio.annotations.ArrayAnnotationService;
+import gov.nih.nci.cabio.annotations.ArrayAnnotationServiceImpl;
+import gov.nih.nci.cabio.domain.ArrayReporter;
+import gov.nih.nci.cabio.domain.Microarray;
+import gov.nih.nci.cabio.portal.portlet.Results;
+import gov.nih.nci.system.applicationservice.CaBioApplicationService;
+import gov.nih.nci.system.client.ApplicationServiceProvider;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+/**
+ * @author <a href="mailto:rokickik@mail.nih.gov">Konrad Rokicki</a>
+ */
+public class ReporterByNameQueryAction extends Action {
+
+    private static Log log = LogFactory.getLog(ReporterByNameQueryAction.class);
+    
+    private CaBioApplicationService as;
+    private ArrayAnnotationService aas;
+    
+    public ReporterByNameQueryAction() throws Exception {
+        this.as = (CaBioApplicationService)
+            ApplicationServiceProvider.getApplicationService();
+        this.aas = new ArrayAnnotationServiceImpl(as);
+    }
+    
+	@Override
+    public ActionForward execute(ActionMapping mapping, ActionForm form, 
+            HttpServletRequest req, HttpServletResponse res) throws Exception {
+
+	    try {
+	        ReporterByNameQueryForm f = (ReporterByNameQueryForm)form;
+	        
+            log.info("microarray: "+f.getMicroarray());
+            log.info("reporter: "+f.getReporterId());
+            log.info("page: "+f.getPage());
+            
+            List<String> reporterIds = new ArrayList<String>();
+            reporterIds.add(f.getReporterId().trim());
+            
+            String arrayPlatform = f.getMicroarray();
+            Microarray microarray = aas.getMicroarray(arrayPlatform);
+            
+            // TODO: move this code into ArrayAnnotationServiceImpl
+            Collection<? extends ArrayReporter> results = null;
+            if ("oligo".equals(microarray.getType())) {
+                results = aas.getExpressionReporterAnnotations(arrayPlatform, reporterIds);
+            }
+            else if ("snp".equals(microarray.getType())) {
+                results = aas.getSNPReporterAnnotations(arrayPlatform, reporterIds);
+            }
+            else if ("exon".equals(microarray.getType())) {
+                results = aas.getExonReporterAnnotations(arrayPlatform, reporterIds);
+            }
+            else {
+                throw new Exception("Unsupported array type: "+microarray.getType());
+            }
+            
+	        req.setAttribute("results", new Results(results, f.getPageNumber()));
+
+            return mapping.findForward("cabioportlet.reporterByNameQuery.results");
+	    }
+	    catch (Exception e) {
+            log.error("Action error",e);
+            req.setAttribute("errorMessage", e.getMessage());
+	        return mapping.findForward("cabioportlet.error");
+	    }
+	}
+}
