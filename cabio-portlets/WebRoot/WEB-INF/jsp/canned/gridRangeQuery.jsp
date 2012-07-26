efdd0c04be981b86d8126950541c98c615e0559c
Sue Pan <sue.pan@nih.gov>
2012-07-26 20:28:42 +0000

    disable grid id range query
    SVN-Revision: 2002

---
+++ cabio-portlets/WebRoot/WEB-INF/jsp/canned/gridRangeQuery.jsp
@@ -0,0 +1,42 @@
+<%@ include file="/WEB-INF/jsp/init.jsp" %>
+
+<div id="queries_range_grid" class="query">
+
+    <html:form action="/cabioportlet/gridRangeQuery">
+    <html:hidden property="page" styleId="page"/>
+
+    <table>
+
+    <tr><td title="Grid identifier (bigid) of a genomic feature"><label for="gridId">Grid Id</label></td><td>
+    <html:text property="gridId" size="40" styleId="gridId"/>
+    </td></tr>
+
+    <tr><td title="Genome assembly"><label for="grid_id_range_queries_assembly">Assembly</label></td><td>
+    <html:select property="assembly" styleId="grid_id_range_queries_assembly">
+    <html:optionsCollection name="globalQueries" property="assemblies" value="value" label="name"/>
+    </html:select>
+    </td></tr>
+    
+    <tr><td title="Distance downstream from the feature to search"><label for="grid_id_range_queries_downstreamPad">Downstream Distance</label></td><td>
+    <html:text property="downstreamPad" size="10" styleId="grid_id_range_queries_downstreamPad"/>
+    </td></tr>
+
+    <tr><td title="Distance upstream from the feature to search"><label for="grid_id_range_queries_upstreamPad">Upstream Distance</label></td><td>
+    <html:text property="upstreamPad" size="10" styleId="grid_id_range_queries_upstreamPad"/>
+    </td></tr>
+
+    <tr><td title="Genomic feature type(s) to view"><label for="grid_id_range_queries_classFilter">Display</label></td><td>
+    <html:select property="classFilter" styleId="grid_id_range_queries_classFilter">
+    <html:option value="">All</html:option>
+    <html:optionsCollection name="globalQueries" property="classFilterValues" 
+                            value="value" label="label"/>
+    </html:select>
+    </td></tr>
+
+    </table>
+
+    <html:submit>Search</html:submit>
+    <html:reset>Reset</html:reset>
+
+    </html:form>
+</div>
---
+++ cabio-portlets/src/gov/nih/nci/cabio/portal/portlet/canned/PathwayByNameQueryAction.java
@@ -0,0 +1,56 @@
+package gov.nih.nci.cabio.portal.portlet.canned;
+
+import gov.nih.nci.cabio.domain.Pathway;
+import gov.nih.nci.cabio.portal.portlet.ReportService;
+import gov.nih.nci.cabio.portal.portlet.Results;
+import gov.nih.nci.system.applicationservice.CaBioApplicationService;
+import gov.nih.nci.system.client.ApplicationServiceProvider;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+public class PathwayByNameQueryAction extends Action {
+
+    private static Log log = LogFactory.getLog(PathwayByNameQueryAction.class);
+
+    private CaBioApplicationService as;
+
+    private ReportService rs;
+
+    public PathwayByNameQueryAction() throws Exception {
+        this.as = (CaBioApplicationService) ApplicationServiceProvider.getApplicationService();
+        this.rs = new ReportService(as);
+    }
+
+    @Override
+    public ActionForward execute(ActionMapping mapping, ActionForm form,
+            HttpServletRequest req, HttpServletResponse res) throws Exception {
+
+        try {
+            PathwayByNameQueryForm f = (PathwayByNameQueryForm) form;
+            String pathwayName = f.getPathwayName();
+
+            log.info("pathwayName: " + pathwayName);
+            log.info("page: " + f.getPage());
+
+            List<Pathway> results = rs.getPathwaysByName(pathwayName);
+
+            req.setAttribute("results", new Results(results, f.getPageNumber()));
+            return mapping.findForward("cabioportlet.pathwayByNameQuery.results");
+        }
+        catch (Exception e) {
+            log.error("Action error", e);
+            req.setAttribute("errorMessage", e.getMessage());
+            return mapping.findForward("cabioportlet.error");
+        }
+    }
+}
