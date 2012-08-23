4e4e9dac2eb53e3a2d1cf7ba4171c7e47d678d91
Sue Pan <sue.pan@nih.gov>
2012-08-23 18:02:41 +0000

    updated URL for the old drug card data
    SVN-Revision: 2023

--- software/cabio-database/scripts/download/download_Drugbank.sh
+++ software/cabio-database/scripts/download/download_Drugbank.sh
@@ -3,5 +3,7 @@ mkpath.pl $CABIO_DATA_DIR/drugbank || exit 1
 cd $CABIO_DATA_DIR/drugbank || exit 1
 rm drugcards.zip
 mv drugcards.txt drugcards.txt.old
-wget -nv http://www.drugbank.ca/public/downloads/current/drugcards.zip
+# please note this version of the drug card doesn't contain the latest drug bank data
+# TODO: retrieve the latest XML format and rewrite the parser
+wget -nv http://www.drugbank.ca/system/downloads/2.5/drugcards.zip
 unzip drugcards.zip
