e2ef476e334cec37b4ba01d32b428f712e7212ec
Sue Pan <sue.pan@nih.gov>
2012-08-23 18:09:45 +0000

    moved the unused scripts to this folder
    SVN-Revision: 2027

--- software/cabio-database/scripts/download/download_pid_data.pl
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/perl
-use warnings;
-
-$machine="nci6116g.nci.nih.gov";
-my $server="\\\\$machine\\L:\\NCICB\\caBIO\\PID_Dump\\Combined Dump 03_10_09";
-my $fullname = "$server\\current.031009";
-print $fullname;
-open my $fh, "<", $fullname or die "$!";
