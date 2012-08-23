76917eaf7ce57a93a2cde5b0ff874577618e67e1
Sue Pan <sue.pan@nih.gov>
2012-08-23 18:08:25 +0000

    added logic to remove empty lines from Hs.data and Mm.data so the existing Java parsers would continue to work
    SVN-Revision: 2025

--- software/cabio-database/scripts/download/download_Unigene_NCBI.sh
+++ software/cabio-database/scripts/download/download_Unigene_NCBI.sh
@@ -9,7 +9,10 @@ wget -nv -O Hs.seq.all.gz ftp://ftp.ncbi.nih.gov/repository/UniGene/Homo_sapiens
 wget -nv -O Hs.data.gz ftp://ftp.ncbi.nih.gov/repository/UniGene/Homo_sapiens/Hs.data.gz  
 echo "Unzipping downloaded file"
 gunzip Hs.seq.all.gz 
-gunzip Hs.data.gz 
+gunzip Hs.data.gz
+# move the empty lines for the Java parser to work
+sed '/^$/d' Hs.data>Hs.data.trim
+mv Hs.data.trim Hs.data
 
 echo "\nDownloading Mouse Unigene sequences"
 wget -nv -O Mm.seq.all.gz ftp://ftp.ncbi.nih.gov/repository/UniGene/Mus_musculus/Mm.seq.all.gz
@@ -17,5 +20,8 @@ wget -nv -O Mm.data.gz ftp://ftp.ncbi.nih.gov/repository/UniGene/Mus_musculus/Mm
 echo "Unzipping downloaded file"
 gunzip Mm.seq.all.gz
 gunzip Mm.data.gz
+# move the empty lines for the Java parser to work
+sed '/^$/d' Mm.data>Mm.data.trim
+mv Mm.data.trim Mm.data
 
 echo "Finished downloading Human and Mouse Unigene sequences"
