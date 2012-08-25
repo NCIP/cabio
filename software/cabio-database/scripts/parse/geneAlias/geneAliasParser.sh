1b1a239b4125709043267cc6e4aaadae589d55bb
Sue Pan <sue.pan@nih.gov>
2012-08-25 13:41:31 +0000

    updated gene_info to gene_info.txt as produced by the download script
    SVN-Revision: 2056

--- software/cabio-database/scripts/parse/geneAlias/geneAliasParser.sh
+++ software/cabio-database/scripts/parse/geneAlias/geneAliasParser.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "Parsing Gene Alias \n"
-perl geneAliasParser.pl gene_info geneAlias.out
+perl geneAliasParser.pl gene_info.txt geneAlias.out
 perl hgncAlias.pl
 perl hgncParser.pl hgncGeneAlias.txt hgncgeneAlias.out
 echo "Finished parsing Gene Alias \n"
