bdb2aea4949b02ba1b59cec5ba56ed8f96a7e341
Sue Pan <sue.pan@nih.gov>
2012-08-25 04:08:06 +0000

    updated MapView ftp URL
    SVN-Revision: 2053

--- software/cabio-database/scripts/download/download_Marker.sh
+++ software/cabio-database/scripts/download/download_Marker.sh
@@ -3,20 +3,25 @@
 mkpath.pl $CABIO_DATA_DIR/marker
 
 cd $CABIO_DATA_DIR/marker/human
-rm -rf *
+rm -rf UniSTS_human.sts
+rm -rf seq_sts.md
+ 
 wget -nv ftp://ftp.ncbi.nlm.nih.gov/repository/UniSTS/UniSTS_human.sts
-wget -nv ftp://ftp.ncbi.nlm.nih.gov/genomes/H_sapiens/mapview/seq_sts.md.gz
+#wget -nv ftp://ftp.ncbi.nlm.nih.gov/genomes/H_sapiens/mapview/seq_sts.md.gz
+wget -nv ftp://ftp.ncbi.nlm.nih.gov/genomes/MapView/Homo_sapiens/sequence/current/initial_release/seq_sts.md.gz 
 #wget -nv ftp://ftp.ncbi.nlm.nih.gov/genomes/H_sapiens/ARCHIVE/BUILD.36.3/mapview/seq_sts.md.gz
-gunzip *
+gunzip *.gz
 
 cd $CABIO_DATA_DIR/marker/mouse
-rm -rf *
+rm -rf UniSTS_mouse.sts
+rm -rf seq_sts.md
+
 wget -nv ftp://ftp.ncbi.nlm.nih.gov/repository/UniSTS/UniSTS_mouse.sts
-wget -nv ftp://ftp.ncbi.nlm.nih.gov/genomes/M_musculus/mapview/seq_sts.md.gz
-gunzip *
+#wget -nv ftp://ftp.ncbi.nlm.nih.gov/genomes/M_musculus/mapview/seq_sts.md.gz
+wget -nv ftp://ftp.ncbi.nlm.nih.gov/genomes/MapView/Mus_musculus/sequence/current/initial_release/seq_sts.md.gz
+gunzip *.gz
 
 cd $CABIO_DATA_DIR/marker
-rm *
+rm UniSTS.* 
 wget -nv ftp://ftp.ncbi.nlm.nih.gov/repository/UniSTS/UniSTS.aliases
 wget -nv ftp://ftp.ncbi.nlm.nih.gov/repository/UniSTS/UniSTS.sts
-
