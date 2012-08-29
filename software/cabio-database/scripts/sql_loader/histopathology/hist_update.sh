1eaa0329b64eeb1fc796749a016b74d834775f9d
Sue Pan <sue.pan@nih.gov>
2012-08-29 02:46:29 +0000

    added logic to create gene_histo.txt
    SVN-Revision: 2059

--- software/cabio-database/scripts/sql_loader/histopathology/hist_update.sh
+++ software/cabio-database/scripts/sql_loader/histopathology/hist_update.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 # Create Gene KW before this
 $SQLPLUS $1 @histCode_preprocess.sql
+# generate gene_histo.txt
+cd $CABIO_DIR/scripts/sql_loader/java
+ant histo
+cd $CABIO_DIR/scripts/sql_loader/histopathology
 #perl splitKeywords.pl
 # load gene_histopathology
 $SQLLDR $1 readsize=1000000 parallel=true control=geneHisto.ctl log=geneHisto.log bad=geneHisto.bad errors=50000 direct=true silent=(HEADER) 
