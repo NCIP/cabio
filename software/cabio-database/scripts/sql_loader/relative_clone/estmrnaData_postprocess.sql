ee9b5abe427639a7178bcc014c31cfc315e0bd54
Sue Pan <sue.pan@nih.gov>
2012-09-20 15:20:52 +0000

    checked in uncommitted changes from the 43 load
    SVN-Revision: 2100

--- software/cabio-database/scripts/sql_loader/relative_clone/estmrnaData_postprocess.sql
+++ software/cabio-database/scripts/sql_loader/relative_clone/estmrnaData_postprocess.sql
@@ -3,6 +3,21 @@ set feedback on;
 set heading on;
 set verify on;
 
+---because UCSC dump file 0 start 1 end coordinates
+update zstg_human_mrna set tstart = tstart + 1;
+commit;
+
+update zstg_human_est set tstart = tstart + 1;
+commit;
+
+update zstg_mouse_mrna set tstart = tstart + 1;
+commit;
+
+update zstg_mouse_est set tstart = tstart + 1;
+commit;
+
+
+
 @$LOAD/indexes/zstg_human_est.cols.sql;
 @$LOAD/indexes/zstg_human_mrna.cols.sql;
 @$LOAD/indexes/zstg_mouse_est.cols.sql;
