8b8549194d1b974f3cf59f60272eb4f0b1fe6045
Sue Pan <sue.pan@nih.gov>
2012-09-24 17:02:28 +0000

    called by loadPathways.sql to resolve a performance issue in loadPathways.sql
    SVN-Revision: 2109

---
+++ software/cabio-database/scripts/sql_loader/sql/BIOGENES.sql
@@ -0,0 +1,57 @@
+Insert into biogenes
+   (bc_id, locus_id, organism, gene_id)
+ Values
+   ('MEF2', 4207, 5, 256624);
+Insert into biogenes
+   (bc_id, locus_id, organism, gene_id)
+ Values
+   ('Mm.TCRA', 21473, 6, 262737);
+Insert into biogenes
+   (bc_id, locus_id, organism, gene_id)
+ Values
+   ('Mm.april', 69583, 6, 227596);
+Insert into biogenes
+   (bc_id, locus_id, organism, gene_id)
+ Values
+   ('Mm.tcra', 21473, 6, 262737);
+Insert into biogenes
+   (bc_id, locus_id, organism, gene_id)
+ Values
+   ('Mm.ugt1a6', 94284, 6, 246480);
+Insert into biogenes
+   (bc_id, locus_id, organism, gene_id)
+ Values
+   ('apo3l', 8742, 5, 251752);
+Insert into biogenes
+   (bc_id, locus_id, organism, gene_id)
+ Values
+   ('ins', 3630, 5, 217386);
+Insert into biogenes
+   (bc_id, locus_id, organism, gene_id)
+ Values
+   ('integrina', 3672, 5, 219089);
+Insert into biogenes
+   (bc_id, locus_id, organism, gene_id)
+ Values
+   ('myt1', 4661, 5, 231640);
+Insert into biogenes
+   (bc_id, locus_id, organism, gene_id)
+ Values
+   ('ndk', 4830, 5, 251821);
+Insert into biogenes
+   (bc_id, locus_id, organism, gene_id)
+ Values
+   ('nme1', 4830, 5, 251821);
+Insert into biogenes
+   (bc_id, locus_id, organism, gene_id)
+ Values
+   ('nr2f1', 7025, 5, 238819);
+Insert into biogenes
+   (bc_id, locus_id, organism, gene_id)
+ Values
+   ('pa', 26258, 5, 212130);
+Insert into biogenes
+   (bc_id, locus_id, organism, gene_id)
+ Values
+   ('ugt1a6', 54578, 5, 251271);
+COMMIT;
